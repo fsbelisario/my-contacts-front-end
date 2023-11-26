7a10f654d9e999b1e5aa4d799a556329c9229df7
Belisario Felipe <fbelisario@gmail.com>
2023-11-26 15:47:45 -0300

    Criação do header da listagem
    Criação do header da listagem

---
+++ src/components/ContactsList/index.js
@@ -0,0 +1,14 @@
+import { Container, Header } from './styles';
+
+export default function ContactsList() {
+  return (
+    <Container>
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href="http://localhost">
+          Novo contato
+        </a>
+      </Header>
+    </Container>
+  );
+}
