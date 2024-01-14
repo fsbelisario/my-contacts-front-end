20b9367dc0e4f8244a4263755698028b954b057c
Belisario Felipe <fbelisario@gmail.com>
2024-01-14 19:17:28 -0300

    Implementação de chamada de carregamento da listagem
    Implementação de chamada de carregamento da listagem pelo botão "Tentar novamente"

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   const [orderBy, setOrderBy] = useState('asc');
   const [searchTerm, setSearchTerm] = useState('');
   const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   const filteredContacts = useMemo(() => contacts.filter((contact) => (
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -43,21 +43,22 @@ export default function Home() {
   //   contact.name.toLowerCase().startsWith(searchTerm.toLowerCase())
   // ));
 
-  useEffect(() => {
-    async function loadContacts() {
-      try {
-        setIsLoading(true);
+  async function loadContacts() {
+    try {
+      setIsLoading(true);
 
-        const contactsList = await ContactsService.listContacts(orderBy);
+      const contactsList = await ContactsService.listContacts(orderBy);
 
-        setContacts(contactsList);
-      } catch {
-        setHasError(true);
-      } finally {
-        setIsLoading(false);
-      }
+      setHasError(false);
+      setContacts(contactsList);
+    } catch {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
+  useEffect(() => {
     loadContacts();
   }, [orderBy]);
 
@@ -69,6 +70,10 @@ export default function Home() {
 
   function handleChangeSearchTerm(event) {
     setSearchTerm(event.target.value);
+  }
+
+  function handleTryAgain() {
+    loadContacts();
   }
 
   return (
@@ -100,48 +105,54 @@ export default function Home() {
           <img src={sad} alt="Sad" />
           <div className="details">
             <strong>Ocorreu um erro ao obter seus contatos!</strong>
-            <Button type="button">
+            <Button type="button" onClick={handleTryAgain}>
               Tentar novamente
             </Button>
           </div>
         </ErrorContainer>
       )}
 
-      {filteredContacts.length > 0 && (
-        <ListHeader orderBy={orderBy}>
-          <button
-            type="button"
-            onClick={handleToggleOrderBy}
-          >
-            <span>Nome</span>
-            <img
-              src={arrow}
-              alt="Arrow icon"
-            />
-          </button>
-        </ListHeader>
+      {!hasError && (
+        <>
+          {filteredContacts.length > 0 && (
+            <ListHeader orderBy={orderBy}>
+              <button
+                type="button"
+                onClick={handleToggleOrderBy}
+              >
+                <span>Nome</span>
+                <img
+                  src={arrow}
+                  alt="Arrow icon"
+                />
+              </button>
+            </ListHeader>
+          )}
+          {filteredContacts.map((contact) => (
+            <Card key={contact.id}>
+              <div className="info">
+                <div className="contact-name">
+                  <strong>{contact.name}</strong>
+                  {contact.category_name && (
+                    <small>{contact.category_name}</small>
+                  )}
+                </div>
+                <span>{contact.email}</span>
+                <span>{formatPhone(contact.phone)}</span>
+              </div>
+              <div className="actions">
+                <Link to={`/edit/${contact.id}`}>
+                  <img src={edit} alt="Edit icon" />
+                </Link>
+                <button type="button">
+                  <img src={trash} alt="Trash icon" />
+                </button>
+              </div>
+            </Card>
+          ))}
+
+        </>
       )}
-      {filteredContacts.map((contact) => (
-        <Card key={contact.id}>
-          <div className="info">
-            <div className="contact-name">
-              <strong>{contact.name}</strong>
-              {contact.category_name
-                && <small>{contact.category_name}</small>}
-            </div>
-            <span>{contact.email}</span>
-            <span>{formatPhone(contact.phone)}</span>
-          </div>
-          <div className="actions">
-            <Link to={`/edit/${contact.id}`}>
-              <img src={edit} alt="Edit icon" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Trash icon" />
-            </button>
-          </div>
-        </Card>
-      ))}
     </Container>
   );
 }
