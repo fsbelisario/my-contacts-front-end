fde9950d896e99ef1d67511bf2462a3026e20cbc
Felipe Belisário <fbelisario@gmail.com>
2024-06-17 21:21:06 -0300

    Implementação do Container/Presentational Pattern
    Implementação do Container/Presentational Pattern

---
+++ src/pages/EditContact/Container.js
@@ -0,0 +1,78 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { useHistory, useParams } from 'react-router-dom';
+
+import Presentation from './Presentation';
+
+import ContactsService from '../../services/ContactsService';
+
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+
+import toast from '../../utils/toast';
+
+export default function Container() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+
+  const history = useHistory();
+
+  const safeAsyncAction = useSafeAsyncAction();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+
+          setContactName(contact.name);
+
+          setIsLoading(false);
+        });
+      } catch {
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado.',
+          });
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history, safeAsyncAction]);
+
+  async function handleSubmit(contact) {
+    try {
+      const updatedContactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(updatedContactData.name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso.',
+        duration: 3000,
+      });
+    } catch (error) {
+      toast({
+        type: 'danger',
+        text: 'Erro ao editar o contato.',
+      });
+    }
+  }
+
+  return (
+    <Presentation
+      isLoading={isLoading}
+      contactName={contactName}
+      contactFormRef={contactFormRef}
+      onSubmit={handleSubmit}
+    />
+  );
+}
