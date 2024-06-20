49dc9d8f8b848a7037abc1f7c2f0fc79b1fe9839
Felipe Belisário <fbelisario@gmail.com>
2024-06-20 20:09:24 -0300

    Animando a entrada do modal com keyframes
    Animando a entrada do modal com keyframes

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,4 +1,24 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from {
+      transform: scale(0);
+  }
+
+  to {
+      transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.6);
@@ -11,6 +31,7 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const Container = styled.div`
@@ -20,6 +41,8 @@ export const Container = styled.div`
   border-radius: 4px;
   padding: 24px;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.04);
+  transform: scale(1);
+  animation: ${scaleIn} 0.3s;
 
   > h1 {
     font-size: 22px;
