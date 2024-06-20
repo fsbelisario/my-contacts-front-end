6da111e93acb19d9f27fd0ed7454e6903cf56cba
Felipe Belisário <fbelisario@gmail.com>
2024-06-19 21:51:15 -0300

    Melhorando a legibilidade das condicionais
    Melhorando a legibilidade das condicionais

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -30,34 +30,35 @@ export default function Home() {
     handleDeleteContact,
   } = useHome();
 
+  const hasContacts = contacts.length > 0;
+  const isListEmpty = !hasError && (!isLoading && !hasContacts);
+  const isSearchEmpty = !hasError && (hasContacts && filteredContacts.length === 0);
+
   return (
     <Container>
       <Loader isLoading={isLoading} />
-      {contacts.length > 0 && (
+
+      {hasContacts && (
         <InputSearch
           value={searchTerm}
           onChange={handleChangeSearchTerm}
         />
       )}
+
       <Header
         hasError={hasError}
         qtyOfContacts={contacts.length}
         qtyOfFilteredContacts={filteredContacts.length}
       />
-      {hasError && (
-        <ErrorStatus onTryAgain={handleTryAgain} />
-      )}
 
-      {!hasError && (
+      {hasError && <ErrorStatus onTryAgain={handleTryAgain} />}
+
+      {isListEmpty && <EmptyList />}
+
+      {isSearchEmpty && <SearchNotFound searchTerm={searchTerm} />}
+
+      {hasContacts && (
         <>
-          {(contacts.length === 0 && !isLoading) && (
-            <EmptyList />
-          )}
-
-          {(contacts.length > 0 && filteredContacts.length === 0) && (
-            <SearchNotFound searchTerm={searchTerm} />
-          )}
-
           <ContactsList
             filteredContacts={filteredContacts}
             orderBy={orderBy}
