c882271615f1f1b790cfcfc08340f39f781133d5
Felipe Belisário <fbelisario@gmail.com>
2024-06-23 19:14:55 -0300

    Adicionando os listeners de animationend
    Adicionando os listeners de animationend

--- src/hooks/useAnimatedList.js
+++ src/hooks/useAnimatedList.js
@@ -1,5 +1,5 @@
 import {
-  createRef, useCallback, useRef, useState,
+  createRef, useCallback, useEffect, useRef, useState,
 } from 'react';
 
 export default function useAnimatedList(initialValue = []) {
@@ -7,15 +7,31 @@ export default function useAnimatedList(initialValue = []) {
   const [pendingRemovalItemsIds, setPendingRemovalItemsIds] = useState([]);
 
   const animatedRefs = useRef(new Map());
+  const animationEndListeners = useRef(new Map());
+
+  const handleAnimationEnd = useCallback((id) => {
+    setItems((prevState) => prevState.filter((message) => message.id !== id));
+    setPendingRemovalItemsIds((prevState) => prevState.filter((messageId) => messageId !== id));
+  }, []);
+
+  useEffect(() => {
+    pendingRemovalItemsIds.forEach((itemId) => {
+      const animatedRef = animatedRefs.current.get(itemId);
+      const alreadyHasListener = animationEndListeners.current.has(itemId);
+
+      if (animatedRef?.current && !alreadyHasListener) {
+        animationEndListeners.current.set(itemId, true);
+
+        animatedRef.current.addEventListener('animationend', () => {
+          handleAnimationEnd(itemId);
+        });
+      }
+    });
+  }, [pendingRemovalItemsIds, handleAnimationEnd]);
 
   const handleRemoveItem = useCallback((id) => {
     setPendingRemovalItemsIds((prevState) => [...prevState, id]);
   }, []);
-
-  // const handleAnimationEnd = useCallback((id) => {
-  //   setItems((prevState) => prevState.filter((message) => message.id !== id));
-  //   setPendingRemovalItemsIds((prevState) => prevState.filter((messageId) => messageId !== id));
-  // }, []);
 
   const getAnimatedRef = useCallback((itemId) => {
     let animatedRef = animatedRefs.current.get(itemId);
